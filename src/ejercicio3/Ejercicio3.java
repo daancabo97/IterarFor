355352f777469d91ea8f313c86145dc1fab7c68c
daancabo97 <danielcaicedo285@gmail.com>
2022-08-14 15:28:30 -0500

    Ejercicio3

---
+++ src/ejercicio3/Ejercicio3.java
@@ -0,0 +1,27 @@
+package ejercicio3;
+
+public class Ejercicio3 {
+
+    public static void main(String[] args) {
+
+     
+        String[] Nombres = {"{ Daniel" + " ," +
+                            "Andres" + " ," +
+                            "Nicol" + " ," + 
+                            "Dayana" + " ," + 
+                            "Maria" + " ," + 
+                            "Juan" + " ," + 
+                            "Carlos" + " ," 
+                            + "Fernanda" +
+                            " ," + "Daniela" + " ," 
+                            + "Claudia" + " ,"
+                            + "Sofia" + " ," + 
+                            "Laura" + " ," + 
+                            "Valentina }" };
+        for (String nombre : Nombres) {
+            System.out.println(nombre);
+
+        }
+    }
+
+}
